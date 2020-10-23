4cee00e7debfd638880fac6dd4046338229a7996
Bhalerao Pritam Nitin <pritam.bhalerao@codevian.com>
2020-10-24 04:13:24 +0530

    port changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 5000;
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
